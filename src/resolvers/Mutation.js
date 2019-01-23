aac5145644d70dfcbac27deabf8df44562228700
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2019-01-23 07:27:36 +0600

    update mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -95,6 +95,28 @@ const Mutation = {
 
     return newPost;
   },
+  updatePost(parent, args, { db} , info) {
+    const {id,data} = args;
+    const post = db.posts.find(post => post.id === id);
+
+    if(!post){
+      throw new Error('Post not found')
+    }
+
+    if(typeof data.title === 'string') {
+      post.title = data.title
+    }
+    
+    if(typeof data.body === 'string') {
+      post.body = data.body
+    }
+    
+    if(typeof data.published === 'boolean') {
+      post.published = data.published
+    }
+    console.log(post)
+    return post
+  },
   deletePost(parent, args, {
     db
   }, info) {
@@ -131,6 +153,20 @@ const Mutation = {
     db.comments.push(newComment);
 
     return newComment;
+  },
+  updateComment(parent, args, {db}, info) {
+    const { id, data} = args;
+    const comment = db.comments.find(comment => comment.id === id);
+
+    if(!comment){
+      throw new Error('Comment not found')
+    }
+
+    if(typeof data.text === 'string'){
+      comment.text = data.text
+    }
+
+    return comment;
   },
   deleteComment(parent, args, {
     db
